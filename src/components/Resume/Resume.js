14e35f9e9ce1b3ae89043be7d9bf873ea6b8ef24
yashagrawal3107 <201851149@iiitvadodara.ac.in>
2021-07-07 21:07:08 +0530

    minor changes

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -1,11 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
 import Resumecontent from "./ResumeContent";
 import "../../style.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import axios from "axios";
+// import axios from "axios";
 import pdf from "../../Assets/Resume_Yash_Agrawal.pdf";
 
 function Resume() {
